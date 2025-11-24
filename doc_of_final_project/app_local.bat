15b8b9304c18afe50bc1f5fa645b272120ed76fd
Anas Ezzine <mekira178@gmail.com>
2025-11-24 02:18:52 +0100

    Remving the doc of final project

--- doc_of_final_project/app_local.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal enabledelayedexpansion
-goto :main
-:main 
-for /l %%i in (1,1,5) do @(curl -L -O "https://elzerowebschool.github.io/HTML_And_CSS_Template_Four/imgs/course-0%%i.jpg")
-endlocal 
